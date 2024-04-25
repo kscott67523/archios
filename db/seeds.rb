d1c69207a1debe3e87921f8bcfa0f530c8977164
kscott67523 <167111437+kscott67523@users.noreply.github.com>
2024-04-25 10:20:54 +0000

    db seed Yoli

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,32 +17,30 @@ company = Company.create!(
 puts "#{Company.count} companies have been created."
 
 # Generate sample data for the employees table
-bob = Employee.create!(
-  first_name: 'Bob',
+yoli = Employee.create!(
+  first_name: "Yolii",
   last_name: Faker::Name.last_name,
   role: :manager,
   company_id: company.id,
   manager_id: nil, # Assign the manager id to the first employee
   phone_number: Faker::Number.number(digits: 10),
-  has_tmail: false,
-  profile_picture: Faker::LoremFlickr.image(size: '50x50'), # Using Faker::LoremPixel
+  profile_picture: "https://pbs.twimg.com/media/FMarwB5WUAUBYgz.jpg:large", # Using Faker::LoremPixel
   time_zone: Faker::Address.time_zone,
-  email: 'bob@example.com',
-  password: 'password'
+  email: "kqscott19@gmail.com",
+  password: "password",
 )
 
-alice = Employee.create!(
-  first_name: 'Alice',
+bob = Employee.create!(
+  first_name: "Bob",
   last_name: Faker::Name.last_name,
   role: :employee,
   company_id: company.id, # Assuming there are 5 companies
-  manager_id: bob.id, # Assign the manager id to the first employee
+  manager_id: yoli.id, # Assign the manager id to the first employee
   phone_number: Faker::Number.number(digits: 10),
-  has_tmail: false,
-  profile_picture: Faker::LoremFlickr.image(size: '50x50'),
+  profile_picture: "https://hips.hearstapps.com/hmg-prod/images/gettyimages-1239961811.jpg?crop=0.668xw:1.00xh;0.250xw,0&resize=640:*",
   time_zone: Faker::Address.time_zone,
-  email: 'alice@example.com',
-  password: 'password'
+  email: "bob@example.com",
+  password: "password",
 )
 
 puts "#{Employee.count} employees have been created."
@@ -51,13 +49,13 @@ puts "#{Employee.count} employees have been created."
 current_pay_period = PayPeriod.create!(
   started_at: Faker::Date.backward(days: 7),
   ended_at: Faker::Date.forward(days: 7),
-  name: Faker::Movies::HarryPotter.spell
+  name: Faker::Movies::HarryPotter.spell,
 )
 
 # Define a range for the typical work hours in a day
 WORK_HOURS_RANGE = (4..10)
 
-# Generate timesheet entries for Alice
+# Generate timesheet entries for Bob
 20.times do
   comments = [Faker::Lorem.sentence, ''].sample
   started_at = Faker::Time.between(from: current_pay_period.started_at, to: current_pay_period.ended_at)
@@ -68,7 +66,7 @@ WORK_HOURS_RANGE = (4..10)
   # Make sure the shift doesn't exceed the pay period's end
   ended_at = [started_at + shift_duration, current_pay_period.ended_at].compact.min
 
-  previous_entry = alice.timesheet_entries.last
+  previous_entry = bob.timesheet_entries.last
 
   if previous_entry && previous_entry.ended_at.nil?
     # If the previous entry hasn't ended, end it randomly within the typical range
@@ -78,14 +76,21 @@ WORK_HOURS_RANGE = (4..10)
   else
     # Create a new entry with calculated start and end times
     TimesheetEntry.create!(
-      employee_id: alice.id,
+      employee_id: bob.id,
       started_at:,
       ended_at:,
       comments:,
-      entry_approval_status: "approved",
-      pay_period_id: current_pay_period.id
+      entry_approval_status: (ended_at.nil? ? "pending" : %w[approved rejected].sample),
+      pay_period_id: current_pay_period.id,
     )
   end
 end
-
 puts "#{TimesheetEntry.count} timesheet entries have been created."
+
+TmailSubscription.create!(
+  employee_id: yoli.id
+)
+
+TmailSubscription.create!(
+  employee_id: bob.id
+)
