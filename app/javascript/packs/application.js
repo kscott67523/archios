c8b59b53df83dcaa84a8ee5a489106359cafe2e7
kscott67523 <167111437+kscott67523@users.noreply.github.com>
2024-04-26 20:47:22 +0000

    rm tmail js file, yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,3 @@ import "bootstrap/dist/js/bootstrap.bundle";
 
 // Load Font Awesome JavaScript
 import "@fortawesome/fontawesome-free/js/all";
-
-// Load your custom JavaScript file
-import "./tmail";
